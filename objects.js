3ac2fea513c0258ced9162359cc79c55d478163e
fosfito <solucionesaltair@yahoo.gr>
2023-11-15 10:26:58 -0300

    Start your engines!

--- objects.js
+++ objects.js
@@ -1,1 +1,26 @@
-console.log("Hola mundo");
+//array de "objetos" persona
+
+const persona1 = {
+    nombre: "Pedro",
+    apellido: "Rodriguez",
+    edad: 45,
+    hijos: ["Felipe", "Mafalda", "Clemente"]
+}
+
+const persona2 = {
+    nombre: "Juan",
+    apellido: "Toloza",
+    edad: 85,
+    hijos: ["Maria", "Nicolas"]
+}
+const persona3 = {
+    nombre: "Juan",
+    apellido: "Toloza",
+    edad: 85,
+    hijos: ["Maria", "Nicolas"]
+}
+
+const personas = [ persona1, persona2 ];
+
+console.log(personas[1]);
+console.table(persona1);
